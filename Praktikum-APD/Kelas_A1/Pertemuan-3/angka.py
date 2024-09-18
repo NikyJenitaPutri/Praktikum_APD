e4480bda059e4cc1cc6491a7194e5583a9736d62
NikyJenitaPutri <jenitaputri25062006@gmail.com>
2024-09-18 19:34:07 +0700

    pertemuan3

---
+++ Praktikum-APD/Kelas_A1/Pertemuan-3/angka.py
@@ -0,0 +1,11 @@
+masukan = int(input("masukan angka: "))
+if masukan == 1:
+    print("1")
+elif masukan == 2:
+    print("2")
+elif masukan == 3:
+    print("3")
+elif masukan == 4:
+    print("4")
+else:
+    print("invalid")
