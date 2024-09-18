b5361d7c56dfa955819bb877aaa6a267c17ececb
NikyJenitaPutri <jenitaputri25062006@gmail.com>
2024-09-18 19:39:09 +0700

    Delete Pertemuan-3 directory

--- Pertemuan-3/cuaca.py
+++
@@ -1,8 +0,0 @@
-cuaca = str(input("masukan cuaca hari ini:"))
-
-if cuaca == "cerah":
-    print("pergi ke sekolah")
-elif cuaca == "hujan":
-    print("tetap di rumah")
-else: 
-    print("invalid")
--- Pertemuan-3/kelinci,kura-kura,kerbau.py
+++
@@ -1,9 +0,0 @@
-a = int(input("masukan angka 1:"))
-b = int(input("masukan angka 2:"))
-
-if a > b:
-    print("kelinci")
-elif a < b:
-    print("kura-kura")
-else:
-    print("kerbau")
