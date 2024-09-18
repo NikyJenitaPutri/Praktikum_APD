498983b3ccec233cecdf32deccd6a342bb57accc
NikyJenitaPutri <jenitaputri25062006@gmail.com>
2024-09-18 19:22:06 +0700

    Add files via upload

---
+++ Pertemuan-3/kelinci,kura-kura,kerbau.py
@@ -0,0 +1,9 @@
+a = int(input("masukan angka 1:"))
+b = int(input("masukan angka 2:"))
+
+if a > b:
+    print("kelinci")
+elif a < b:
+    print("kura-kura")
+else:
+    print("kerbau")
---
+++ Pertemuan-3/sapi, singa, invalid.py
@@ -0,0 +1,8 @@
+x = int(input("masukan x:"))
+
+if x > 30:
+    print("sapi")
+    if x > 50:
+        print("singa")
+else:
+        print("invalid")    
